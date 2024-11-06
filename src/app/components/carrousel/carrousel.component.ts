da4ff0810075c74944a964d1edb4eabdb6792933
Bajan1408 <gianrodrigues@gmail.com>
2024-11-06 20:45:03 -0300

    fix-security: updates dependencies

--- src/app/components/carrousel/carrousel.component.ts
+++ src/app/components/carrousel/carrousel.component.ts
@@ -53,62 +53,6 @@
 
 */
 
-// import { Component, OnInit } from '@angular/core';
-
-// export interface Image {
-//   src: string;
-//   selected: boolean;
-// }
-
-// @Component({
-//   selector: 'app-carrousel',
-//   templateUrl: './carrousel.component.html',
-//   styleUrls: ['./carrousel.component.css']
-// })
-// export class CarrouselComponent implements OnInit {
-//   time: number = 4000;
-//   currentImgIndex: number = 0;
-//   images: Image[] = [
-//     { src: "assets/img/1a_etapa2023.jpg", selected: true },
-//     { src: "assets/img/foto_atleta.jpg", selected: false },
-//     { src: "assets/img/2a_etapa2023.jpg", selected: false },
-//     { src: "assets/img/4a_etapa2023.jpg", selected: false },
-//     { src: "assets/img/foto_atleta.jpg", selected: false }
-//   ];
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//     this.start();
-//   }
-
-//   updateVisibleImages(): void {
-//     this.images.forEach((image, index) => {
-//       image.selected = index === this.currentImgIndex;
-//     });
-//     const offset = -100 / 3 * this.currentImgIndex;
-//     document.querySelector('.carousel')!.style.transform = `translateX(${offset}%)`;
-//   }
-
-//   nextImage(): void {
-//     this.currentImgIndex = (this.currentImgIndex + 1) % this.images.length;
-//     this.updateVisibleImages();
-//   }
-
-//   previousImage(): void {
-//     this.currentImgIndex = (this.currentImgIndex - 1 + this.images.length) % this.images.length;
-//     this.updateVisibleImages();
-//   }
-
-//   start(): void {
-//     setInterval(() => {
-//       this.nextImage();
-//     }, this.time);
-//   }
-// }
-
-
-
 
 import { Component, ViewChild, ElementRef, OnInit, asNativeElements } from '@angular/core';
 
@@ -128,21 +72,21 @@ export class CarrouselComponent implements OnInit {
   time: number = 2000;
   currentImgIndex: number = 0;
   images: Image[] = [
-    { src: "assets/img/1a_etapa2023.jpg", selected: true, center: true },
-    { src: "assets/img/foto_atleta.jpg", selected: false, center: false },
-    { src: "assets/img/2a_etapa2023.jpg", selected: false, center: false },
-    { src: "assets/img/4a_etapa2023.jpg", selected: false, center: false },
-    { src: "assets/img/foto_atleta.jpg", selected: false, center: false },
-    { src: "assets/img/1a_etapa2023.jpg", selected: true, center: true },
-    { src: "assets/img/foto_atleta.jpg", selected: false, center: false },
-    { src: "assets/img/2a_etapa2023.jpg", selected: false, center: false },
-    { src: "assets/img/4a_etapa2023.jpg", selected: false, center: false },
-    { src: "assets/img/foto_atleta.jpg", selected: false, center: false },
-    { src: "assets/img/1a_etapa2023.jpg", selected: true, center: true },
+    { src: "assets/img/1a_etapa2023.jpg", selected: false, center: false },
     { src: "assets/img/foto_atleta.jpg", selected: false, center: false },
     { src: "assets/img/2a_etapa2023.jpg", selected: false, center: false },
     { src: "assets/img/4a_etapa2023.jpg", selected: false, center: false },
     { src: "assets/img/foto_atleta.jpg", selected: false, center: false }
+    // { src: "assets/img/1a_etapa2023.jpg", selected: true, center: true },
+    // { src: "assets/img/foto_atleta.jpg", selected: false, center: false },
+    // { src: "assets/img/2a_etapa2023.jpg", selected: false, center: false },
+    // { src: "assets/img/4a_etapa2023.jpg", selected: false, center: false },
+    // { src: "assets/img/foto_atleta.jpg", selected: false, center: false },
+    // { src: "assets/img/1a_etapa2023.jpg", selected: true, center: true },
+    // { src: "assets/img/foto_atleta.jpg", selected: false, center: false },
+    // { src: "assets/img/2a_etapa2023.jpg", selected: false, center: false },
+    // { src: "assets/img/4a_etapa2023.jpg", selected: false, center: false },
+    // { src: "assets/img/foto_atleta.jpg", selected: false, center: false }
   ];
 
   @ViewChild('carousel') carousel!: ElementRef;
@@ -162,7 +106,7 @@ export class CarrouselComponent implements OnInit {
     if(this.carousel && this.carousel.nativeElement) {
       
       if(offset == -100) {
-        offset = 33;
+        offset = 0;
       }
       this.carousel.nativeElement.style.transform = `translateX(${offset}%)`;
       //this.carousel.nativeElement.style.width = `124rem`;
@@ -187,4 +131,3 @@ export class CarrouselComponent implements OnInit {
     }, this.time);
   }
 }
-
